0ad2dacc7fdfb5c7be71f58b4d896d56a971100f
Anna Kobets <ank4kobets@mail.ru>
2025-01-20 09:12:08 +0300

    init

---
+++ Logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anna on 1/20/25.
+//
+
+#include "Logger.h"
---
+++ Logger.h
@@ -0,0 +1,16 @@
+//
+// Created by anna on 1/20/25.
+//
+
+#ifndef LOGGER_H
+#define LOGGER_H
+
+
+
+class Logger {
+
+};
+
+
+
+#endif //LOGGER_H
