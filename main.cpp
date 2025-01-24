46e3c3ec7326be95584daa528ab63a958ed13d55
ankobets <ank4kobets@yandex.ru>
2025-01-25 00:34:51 +0300

    Adding main args

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
+#include <iostream>
+
 #include "Logger.h"
 
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc < 3)
+    {
+        std::cerr << "Usage: " << argv[0]<<"Usage: <log file> <log level>";
+    }
+
+    std::string log_file = argv[1];
+    std::string log_level = argv[2];
+
     Logger temp("file.txt", Logger::Level::INFO);
 
     temp.log(Logger::Level::ERROR, "Hello World!");
