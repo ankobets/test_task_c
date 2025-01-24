58eb6e420b622c46e4008e42765fc5ceaa2a5e5d
ankobets <ank4kobets@yandex.ru>
2025-01-24 23:42:22 +0300

    Initial commit

--- Logger.cpp
+++ Logger.cpp
@@ -3,3 +3,69 @@
 //
 
 #include "Logger.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <iomanip>
+
+Logger::Logger (const std::string& fileName, const Level lvl)
+{
+    logLevel = lvl;
+
+    if (!logFile)
+    {
+        throw std::runtime_error("Could not open log file.");
+    }
+
+    logFile.open(fileName, std::ios::app);
+}
+
+Logger::~Logger()
+{
+    logFile.close();
+}
+
+void Logger::log(const Level lvl, const std::string& message)
+{
+    std::stringstream ss;
+    if (lvl < logLevel) return;
+
+    ss<<"["<<getCurrentTime()<<"] "<<"["<<getLogLevel(lvl)<<"] "<<message<<std::endl;
+    logFile<<ss.str();
+
+
+}
+
+void Logger::setLogLevel(const Level lvl)
+{
+    logLevel = lvl;
+}
+
+std::string Logger::getCurrentTime()
+{
+    auto currentTime = std::chrono::system_clock::now();
+    auto tt = std::chrono::system_clock::to_time_t(currentTime);
+
+
+    std::stringstream ss;
+    ss << std::put_time(std::localtime(&tt), "%F %T");
+    std::string st = ss.str();
+
+    return st;
+
+}
+
+std::string Logger::getLogLevel(const Level lvl)
+{
+    switch (lvl)
+    {
+        case Level::INFO:
+            return "INFO";
+        case Level::WARNING:
+            return "WARNING";
+        case Level::ERROR:
+            return "ERROR";
+
+    }
+}
--- Logger.h
+++ Logger.h
@@ -4,11 +4,30 @@
 
 #ifndef LOGGER_H
 #define LOGGER_H
+#include <string>
+#include <fstream>
 
 
+class Logger
+{
+public:
+    enum class Level
+    {
+        INFO, WARNING, ERROR
+    };
 
-class Logger {
+    Logger (const std::string& fileName, Level lvl);
+    ~Logger();
+    void log (Level lvl, const std::string& msg);
+    void setLogLevel(Level lvl);
 
+
+private:
+    std::fstream logFile;
+    Level logLevel;
+
+    std::string getCurrentTime();
+    std::string getLogLevel(Level lvl);
 };
 
 
