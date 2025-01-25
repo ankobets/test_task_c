15ddc0bfd2d7e35cb29b853d7b89d2987994fb90
ankobets <140799191+ankobets@users.noreply.github.com>
2025-01-25 23:36:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 #include "Logger.h"
-#include <limits>
 
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-[[noreturn]] int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
@@ -53,8 +50,3 @@
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
